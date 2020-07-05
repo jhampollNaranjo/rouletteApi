f6fb5dbd2dff45f2a81de4f4901d4378ef3bedb4
JhampollNaranjo <jhampollnaranjo@gmail.com>
2020-07-05 15:42:10 -0500

    Endpoint de apertura de ruleta (el input es un id de ruleta) que permita las posteriores peticiones de apuestas, este debe devolver simplemente un estado que confirme que la operación fue exitosa o denegada

--- src/routes/rouletteRouter.ts
+++ src/routes/rouletteRouter.ts
@@ -1,6 +1,5 @@
 import { Router, Request, Response } from 'express';
 import model from '../model/roulettemodel';
-
 class rouletteRouter {
     public router: Router;
 
@@ -8,30 +7,22 @@ class rouletteRouter {
         this.router = Router();
         this.routes();
     }
-
-    public getRulette(req:Request,res:Response) {
- 
+    public getRulette(req:Request,res:Response) { 
     }
     public createRulette(req:Request,res:Response){
         model.CreateRulette(req,res);
     }
     public OpeningRulette(req:Request,res:Response){
-
-
+        model.OpeningRulette(req,res);
     }
     public OpeningBet(req:Request,res:Response){
-
-
     }
     public closebet(req:Request,res:Response){
-
     }
     public routes() {
         this.router.post('/add', this.createRulette);
-           
+        this.router.put('/opening/:id', this.OpeningRulette);      
     }
 }
-
 const rulette = new rouletteRouter();
-
 export default rulette.router;
