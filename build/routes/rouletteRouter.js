7ed4a9541e7eaf17950ff255a9506c9f278ee5bc
JhampollNaranjo <jhampollnaranjo@gmail.com>
2020-07-05 15:20:04 -0500

    add rouletteRouter class  router

---
+++ build/routes/rouletteRouter.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+class rouletteRouter {
+    constructor() {
+        this.router = express_1.Router();
+    }
+    getRulette(req, res) {
+    }
+    createRulette(req, res) {
+    }
+    OpeningRulette(req, res) {
+    }
+    OpeningBet(req, res) {
+    }
+    closebet(req, res) {
+    }
+    routes() {
+    }
+}
+const ruleteoutes = new rouletteRouter();
+exports.default = ruleteoutes.router;
---
+++ src/routes/rouletteRouter.ts
@@ -0,0 +1,36 @@
+import { Router, Request, Response } from 'express';
+
+
+
+class rouletteRouter {
+    router: Router;
+    constructor() {
+        this.router = Router();
+    }
+
+    getRulette(req:Request,res:Response) {
+    }
+    createRulette(req:Request,res:Response){
+      
+    }
+    OpeningRulette(req:Request,res:Response){
+     
+
+    }
+    OpeningBet(req:Request,res:Response){
+    
+
+    }
+    closebet(req:Request,res:Response){
+   
+    }
+
+
+    routes() {
+     
+    }
+}
+
+const ruleteoutes = new rouletteRouter();
+
+export default ruleteoutes.router;
