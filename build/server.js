31913c5d854b4ecb7c40ad6ff8a9fc14a5a2ca25
JhampollNaranjo <jhampollnaranjo@gmail.com>
2020-07-05 15:08:42 -0500

    connecting server

---
+++ build/server.js
@@ -0,0 +1,33 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const helmet_1 = __importDefault(require("helmet"));
+const cors_1 = __importDefault(require("cors"));
+class Server {
+    constructor() {
+        this.app = express_1.default();
+        this.config();
+        this.routes();
+    }
+    config() {
+        this.app.set('port', process.env.PORT || 3000);
+        this.app.use(morgan_1.default('dev'));
+        this.app.use(express_1.default.json());
+        this.app.use(express_1.default.urlencoded({ extended: false }));
+        this.app.use(helmet_1.default());
+        this.app.use(cors_1.default());
+    }
+    routes() {
+    }
+    start() {
+        this.app.listen(this.app.get('port'), () => {
+            console.log('Server is listenning on port', this.app.get('port'));
+        });
+    }
+}
+const server = new Server();
+server.start();
---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+
+class Server {
+    public app: express.Application;
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+    public config(): void {
+
+        this.app.set('port', process.env.PORT || 3000);
+        this.app.use(morgan('dev'));
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended: false}));
+        this.app.use(helmet());
+        this.app.use(cors());
+    }
+    public routes(): void {
+        
+    }
+    public start(): void {
+        this.app.listen(this.app.get('port'), () => {
+            console.log('Server is listenning on port', this.app.get('port'));
+        });
+    }
+}
+const server = new Server();
+server.start();
